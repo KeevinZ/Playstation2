e667c18c990baa00cdcc7e4d756a7e001095a71e
TechUsopp <kevin1080p.mdm@gmail.com>
2024-09-26 12:34:03 -0300

    Adicionando a String Cor na Class Genero

--- Playstation2/Models/Genero.cs
+++ Playstation2/Models/Genero.cs
@@ -8,9 +8,13 @@ namespace Playstation2.Models
         [Key]
         public int GeneroID { get; set; }
 
-        [Required]
-        [StringLength(100)]
-        public string NomeGenero { get; set; }
+    [Required(ErrorMessage = "Informe a gênero")]
+    [StringLength(25, ErrorMessage = "O gênero deve possuir no máximo 25 caracteres")]
+    public string NomeGenero { get; set; }
+
+    [Required(ErrorMessage = "Informe a cor")]
+    [StringLength(25, ErrorMessage = "A Cor deve possuir no máximo 25 caracteres")]
+    public string Cor { get; set; }
 
         public ICollection<JogoGenero> JogoGeneros { get; set; } = new List<JogoGenero>();
     }
